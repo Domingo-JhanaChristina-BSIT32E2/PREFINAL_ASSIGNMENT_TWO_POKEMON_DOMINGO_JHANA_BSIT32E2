07a559183c38b6404ad487f5964f2a1d6af9ea23
Domingo-Jhana Christina L.-BSIT 32E2 <148880134+Domingo-JhanaChristina-BSIT32E2@users.noreply.github.com>
2024-05-30 17:14:54 +0800

    Add project files.

---
+++ PREFINAL_ASSIGNMENT_TWO_POKEMON_DOMINGO_JHANA_BSIT32E2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_DOMINGO_JHANA_BSIT32E2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
